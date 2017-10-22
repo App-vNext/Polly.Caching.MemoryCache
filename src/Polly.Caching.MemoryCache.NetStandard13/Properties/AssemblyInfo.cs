ffc56d45b755502e8d435b54ef203ed40f4679cf
reisenberger <software@reisenberger.net>
2017-10-22 14:15:38 +0100

    Fix build script
    Fix build script for updating NetStandard Assembly version number

--- src/Polly.Caching.MemoryCache.NetStandard13/Properties/AssemblyInfo.cs
+++ src/Polly.Caching.MemoryCache.NetStandard13/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Polly.Caching.MemoryCache")]
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: CLSCompliant(false)] // Because Microsoft.Extensions.Caching.Memory.MemoryCache, on which Polly.Caching.MemoryCache.NetStandard13 depends, is not CLSCompliant.
 
 [assembly: InternalsVisibleTo("Polly.Caching.MemoryCache.NetStandard13.Specs")]
