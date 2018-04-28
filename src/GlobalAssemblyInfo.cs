ea8b97534ede641459b2393c42a9729e979074ca
reisenberger <software@reisenberger.net>
2018-04-28 16:14:56 +0100

    Nuspec formatting and copyright year

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("Polly.Caching.MemoryCache")]
 [assembly: AssemblyCompany("App vNext")]
 [assembly: AssemblyDescription("Polly.Caching.MemoryCache is a MemoryCache plug-in for the Polly CachePolicy.  Polly is a library that allows developers to express resilience and transient fault handling policies such as Retry, Circuit Breaker, Timeout, Bulkhead Isolation, and Fallback in a fluent and thread-safe manner.")]
-[assembly: AssemblyCopyright("Copyright (c) 2017, App vNext")]
+[assembly: AssemblyCopyright("Copyright (c) 2018, App vNext")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
