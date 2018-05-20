4ae114ddaf452053b8fc8178cb2022d6d166693e
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2018-05-20 09:57:59 +0100

    Remove the cache suffix from any assemblies/projects that end with MemoryCache, as the name Polly.Caching.Memory is descriptive enough and won't cause namespace clashes with any microsoft libraries.

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 
-[assembly: AssemblyProduct("Polly.Caching.MemoryCache")]
+[assembly: AssemblyProduct("Polly.Caching.Memory")]
 [assembly: AssemblyCompany("App vNext")]
-[assembly: AssemblyDescription("Polly.Caching.MemoryCache is a MemoryCache plug-in for the Polly CachePolicy.  Polly is a library that allows developers to express resilience and transient fault handling policies such as Retry, Circuit Breaker, Timeout, Bulkhead Isolation, and Fallback in a fluent and thread-safe manner.")]
+[assembly: AssemblyDescription("Polly.Caching.Memory is a MemoryCache plug-in for the Polly CachePolicy.  Polly is a library that allows developers to express resilience and transient fault handling policies such as Retry, Circuit Breaker, Timeout, Bulkhead Isolation, and Fallback in a fluent and thread-safe manner.")]
 [assembly: AssemblyCopyright("Copyright (c) 2018, App vNext")]
 
 #if DEBUG
--- src/Polly.Caching.Memory.NetStandard13/Properties/AssemblyInfo.cs
+++ src/Polly.Caching.Memory.NetStandard13/Properties/AssemblyInfo.cs
@@ -2,8 +2,8 @@ using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("Polly.Caching.MemoryCache")]
+[assembly: AssemblyTitle("Polly.Caching.Memory")]
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: CLSCompliant(false)] // Because Microsoft.Extensions.Caching.Memory.MemoryCache, on which Polly.Caching.MemoryCache.NetStandard13 depends, is not CLSCompliant.
 
-[assembly: InternalsVisibleTo("Polly.Caching.MemoryCache.NetStandard13-Signed.Specs")]
+[assembly: InternalsVisibleTo("Polly.Caching.Memory.NetStandard13.Specs")]
