05a7a17dfc6d74f9387d3917963b60a60f253645
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2018-05-20 07:22:13 +0100

    Duplicate the .net standard v1.3 signed implementation and specs projects for dotnet standard v2.

---
+++ src/Polly.Caching.MemoryCache.NetStandard20.Specs/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using Xunit;
+
+[assembly: AssemblyTitle("Polly.Caching.MemoryCache.NetStandard13.Specs")]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
