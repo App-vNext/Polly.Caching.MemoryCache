8c578965c65e2a2691615f6ae70dcd503e70eb79
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2018-05-20 07:32:00 +0100

    Remove the .net 4 and 4.5 sync and async project variants along with the .net standard 1.3 unsigned projects.

--- src/Polly.Caching.MemoryCache.Net40Async-Signed/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Polly.Caching.MemoryCache")]
-[assembly: CLSCompliant(false)] // Because Nito.AsycEx, on which Polly.Net40Async depends, is not CLSCompliant.
-
-
-[assembly: InternalsVisibleTo("Polly.Caching.MemoryCache.Net40Async-Signed.Specs")]
--- src/Polly.Caching.MemoryCache.NetStandard13.Specs/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using Xunit;
-
-[assembly: AssemblyTitle("Polly.Caching.MemoryCache.NetStandard13.Specs")]
-[assembly: CollectionBehavior(DisableTestParallelization = true)]
