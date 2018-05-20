455087d7c8ced52187d3ca8dd8d729fc6cda071a
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2018-05-20 07:22:13 +0100

    Duplicate the .net standard v1.3 signed implementation and specs projects for dotnet standard v2.

---
+++ src/Polly.Caching.MemoryCache.NetStandard20/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Polly.Caching.MemoryCache")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: CLSCompliant(false)] // Because Microsoft.Extensions.Caching.Memory.MemoryCache, on which Polly.Caching.MemoryCache.NetStandard13 depends, is not CLSCompliant.
+
+[assembly: InternalsVisibleTo("Polly.Caching.MemoryCache.NetStandard13-Signed.Specs")]
