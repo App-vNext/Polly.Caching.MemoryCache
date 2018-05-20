adc0fb4e23b8337f76d6abc5daa91eea89b98af6
Sean Farrow <sean.farrow@seanfarrow.co.uk>
2018-05-20 16:08:20 +0100

    Update the cake build script to have the same structure as the script in the Polly.Extensions.Http repository. Also, update the assembly info files accordingly.

--- src/Polly.Caching.Memory.NetStandard13/Properties/AssemblyInfo.cs
+++ src/Polly.Caching.Memory.NetStandard13/Properties/AssemblyInfo.cs
@@ -3,7 +3,9 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("Polly.Caching.Memory")]
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
 [assembly: CLSCompliant(false)] // Because Microsoft.Extensions.Caching.Memory.MemoryCache, on which Polly.Caching.MemoryCache.NetStandard13 depends, is not CLSCompliant.
 
 [assembly: InternalsVisibleTo("Polly.Caching.Memory.NetStandard13.Specs")]
