b91c9016da5e3410419bbab40c27c574ef0a52f2
reisenberger <software@reisenberger.net>
2019-01-26 12:07:18 +0000

    Update copyright year

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("Polly.Caching.Memory")]
 [assembly: AssemblyCompany("App vNext")]
 [assembly: AssemblyDescription("Polly.Caching.Memory is a MemoryCache plug-in for the Polly CachePolicy.  Polly is a library that allows developers to express resilience and transient fault handling policies such as Retry, Circuit Breaker, Timeout, Bulkhead Isolation, and Fallback in a fluent and thread-safe manner.")]
-[assembly: AssemblyCopyright("Copyright (c) 2018, App vNext")]
+[assembly: AssemblyCopyright("Copyright (c) 2019, App vNext")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
